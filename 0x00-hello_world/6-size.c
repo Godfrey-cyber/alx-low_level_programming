516eaabec15279e9fa2b7f1b9e5cfe5e2ea20518
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-20 13:09:09 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ float floating_num;
 long longnum;
 long long longint;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(name));
+	printf("Size of a char: %lu byte(s)\n", sizeof(name));
 	printf("Size of an int: %lu byte(s)\n", sizeof(num));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longnum));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(longint));
