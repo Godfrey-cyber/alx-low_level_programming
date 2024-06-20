3f656aee5451c0fad0cac3328c600dd6b337f682
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-20 12:47:24 -0700

    hello comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void) {
+/**
+ * main - Entry point of the c code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
