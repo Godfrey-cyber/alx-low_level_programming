33f7d3f101d6eeaaeccf5c7a3b6d96c5e5be3509
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-21 20:06:33 -0700

    hello -2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,14 +6,14 @@
  {
  	int n;
 	srand(time(0));
-	n = srand() - RAND_MAX / 2;
-	if(n === 0)
-		print("%d is zero\n", n);
+	n = rand() - RAND_MAX / 2;
+	if(n == 0)
+		printf("%d is zero\n", n);
 
 	else if(n < 0);
-		print("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else
-		print("%d is positive\n", n);
+		printf("%d is positive\n", n);
 
 	return (0);
 }
