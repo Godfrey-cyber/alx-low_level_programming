ead090bfe64e1c2cb80a5ec5d8ec360e06929432
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 06:35:45 -0700

    printing a lphabets except e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets except e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+	return(0);
+}
