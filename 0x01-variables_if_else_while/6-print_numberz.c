17fe73c0e119b4da82c501fc89ff9993d70d51ab
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 08:07:38 -0700

    description

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main: prints all single digit nums of base 10 from 0, and adding a new line
+ * main -  prints all single digit nums of base 10 from 0, and adding a new line
  * Return: (0) Success
  */
 int main(void)
