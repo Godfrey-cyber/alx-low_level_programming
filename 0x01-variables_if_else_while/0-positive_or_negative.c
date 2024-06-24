3b1c26d1262aee2d1f349f4e94124cf0e93ae5c1
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 00:44:23 -0700

    helloo-three

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,16 @@
  {
 	/*This program will assign a random number to the variable n each time it is executed.*/
  	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/*logic goes in here*/
 	if (n == 0)
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	else if (n < 0)
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 
 	return (0);
-}
+ }
