c4ccc50377577c842c0f44951c782c11a5de6c8e
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 11:00:29 -0700

    program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ * Return: Returns (0) 
+ */
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers <= 57)
+	{
+		putchar(numbers);
+		if (numbers != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		numbers++
+	}
+	putchar('\n');
+
+	return (0);
+}
