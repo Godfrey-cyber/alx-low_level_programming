518e5775f45e42dedf2dd8f1c36a58d96af66705
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 01:51:27 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the code
+ * Return: Always 0 (Success) -> 0
+ */
+int main_alpha(void)
+{
+
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+        putchar('\n');
+
+return (0);
+}
