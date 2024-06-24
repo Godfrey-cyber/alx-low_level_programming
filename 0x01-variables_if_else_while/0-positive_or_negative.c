d456b1365d0f9f8b125421540992befe509c69c9
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 03:53:32 -0700

    - or +

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,14 @@
 
 /* betty style doc for function main goes there */
 
- int main(void)
- {
-	/*This program will assign a random number to the variable n each time it is executed.*/
- 	int n;
-
+int main(void)
+}
+	int n;
+	
 	srand(time(0));
+	
 	n = rand() - RAND_MAX / 2;
-	/*logic goes in here*/
+
 	if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
@@ -20,5 +20,5 @@
 	else
 		printf("%d is positive\n", n);
 
-	return (0);
- }
+		return (0);
+}
