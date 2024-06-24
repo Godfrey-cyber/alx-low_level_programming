d565e1e41d48bb4dae5b52efa93da8a606f0531c
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 05:55:13 -0700

    correcction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 	char lower_letter;
-    	char upper_letter;
+	char upper_letter;
 
 	for (lower_letter = 97; lower_letter <= 122; lower_letter++)
 	{
-        	putchar(lower_letter);
+		putchar(lower_letter);
 	}
 
 	for (upper_letter = 'A'; upper_letter <= 'Z'; upper_letter++)
-	{
+	}
 		putchar(upper_letter);
 	}
-       putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
