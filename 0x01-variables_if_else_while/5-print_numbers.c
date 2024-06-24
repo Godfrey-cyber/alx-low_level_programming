f857ec780291e7472a3407183fb1f70dedf84bf0
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-24 06:55:41 -0700

    printing nums to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/** main -  a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Return - (0) Success
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10 digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
