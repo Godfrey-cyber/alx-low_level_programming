a73608ba65969ca3e30e443183cf169d1bb6f546
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-25 04:50:30 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
-#include <main.h>
+#include "myname.h"
 /**
  * main - a program that prints _putchar, followed by a new line.
  * Return: Returns (0) Success
  */
 int main(void)
-{
-	printf("_putchar");
+{	
+	int i;
+	char name[] = "Godfrey";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(p[i]);
+	}
 	putchar("\n");
 	return 0;
 }
