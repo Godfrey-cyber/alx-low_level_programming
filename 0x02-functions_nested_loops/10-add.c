53c97232fab589fd3d5f189e883bbfb4c35d2fc3
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-26 12:38:44 -0700

    Add of number one and number two

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * add -  a function that adds two integers and returns the result.
- *  @a: number one.
- *  @b: number two.
+ *  @num1: number one
+ *  @num2: number two
  * Return: Add of number one and number two
  */
 int add(int num1, int num2)
