3032651536c73064189e27dfe20295b59a3cb70a
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-25 20:08:54 -0700

    Check if character is an alphabetic character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - is Shows 1 if the input is a lowercase character
  * @c: The character in ASCII code
- * Return - 1 for lowercase. 0 for the rest.
+ * Return - Returns 1 for lowercase. 0 for the rest.
  */
 int _islower(int c)
 {
