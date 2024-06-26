5f65841c16dc8a8792bebe677568e15b329905e8
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-25 20:16:32 -0700

    etermines if the input number * greater, iqual or less than zero.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - Checks if the input number is
+* >, = or < 0.
+*
+* @n: The input number as an integer.
+*
+* Return: 1 is > 0. 0 is zero.
+* -1 is > 0.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		{
+		_putchar(43);
+	return (1);
+		}
+	else if (n < 0)
+		{
+		_putchar(45);
+	return (-1);
+		}
+	else
+		{
+		_putchar(48);
+	return (0);
+		}
+		_putchar('\n');	
+}
