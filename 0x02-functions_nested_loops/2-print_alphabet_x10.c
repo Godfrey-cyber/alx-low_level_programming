265592c684e2888bd11e4eb0c9647a96869a1de1
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-25 19:38:32 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - a function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i <= 10; i++)
+	{
+		char letter;
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	_putchar("\n");
+	}
+}
