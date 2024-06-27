61870d0d54d6a198fe92ff761e2e29ffc6de3f80
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-27 13:28:52 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers -	a function that prints the numbers,
+ *  from 0 to 9, followed by a new line.
+ *  Return: Returns all numbers except 2 & 4
+ */
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 0; num <= 10; num++)
+	{
+		if (num !== 2 || num !== 4)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+}
