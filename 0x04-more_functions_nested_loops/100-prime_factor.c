3804f16fb75ab6daa3d0bd0c93422e65f07b1dee
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-06-27 19:51:46 -0700

    a program that finds and prints the largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry block of the code
+* @void: no argument for execution
+* Return: 0 
+*/
+int main(void)
+{
+	long i, number = 612852475143;
+
+	for (i  = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
