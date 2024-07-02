e0bd60d082e41ca935aee156ade78cdf31a78570
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-02 12:39:48 -0700

    correction-error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *  puts - prints the characters of the string
+ *  puts2 - prints the characters of the string
  * @str: char to check
  * Return: Always 0.
  */
 void puts2(char *str)
 {
 	int string;
-
+	
 	for (string = 0; str[string] != '\0'; string++)
 	if (string % 2 == 0)
 		_putchar(str[string]);
