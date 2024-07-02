c624cdae25a1670546fa91ac177b658aab28f938
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-01 19:49:51 -0700

    prints he string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <sstdio.h>
+#include "main.h"
+/**
+ * * main - prints the characters of the string
+ * *
+ * * Return: Always 0.
+ * */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
