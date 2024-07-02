07b489cc38b9e18d6bcaab262bdf692afe9696b4
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-02 11:25:05 -0700

    correction of error

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * swap_int - a function tht swaps integers
- * @a - int to check
- * @b - second int to check
+ * @a: int to check
+ * @b: second int to check
  */
 void swap_int(int *a, int *b)
 {
