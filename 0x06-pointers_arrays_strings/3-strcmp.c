515de654a973dbc6099b14f214dd6191ccbafbcc
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-04 02:14:43 -0700

    int _strcmp(char *s1, char *s2)

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - this function compares two strings
+ * @string1: char to check
+ * @string2: char to check
+ * Return: 0 is success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int num;
+
+	num = 0;
+	while (num < n && src[num] != '\0')
+	{
+		dest[num] = src[num];
+		j++;
+	}
+	while (num < n)
+	{
+		dest[num] = '\0';
+		num++;
+	}
+	return (dest);
+}
