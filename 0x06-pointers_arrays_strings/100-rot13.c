0ee6c0fe0b6c9d63106a2470f098b331c27f1aec
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-04 13:48:27 -0700

    error correction

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,39 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* rot13 - encodes a string using rot13 encryption
-* @s: a pointer given by main
-* Description: change all alpha characters by rot13 rules
-* Return: returns the s pointer
-*/
-
-char *rot13(char *s)
+ * *rot13 - encodes a string using rot13
+ *
+ * @rot: int to check
+ *
+ * Return: 0 is success
+ */
+char *rot13(char *rot)
 {
-	int i = 0;
+	int a, b;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	int j;
-
-	int begin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
-	'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-	'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-	int end[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-	'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O',
-	'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
-	'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-
-	while (s[i] != 0)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == begin[j])
+	for (a = 0; rot[a] != '\0'; a++)
+		for (b = 0; input[b] != '\0'; b++)
+			if (rot[a] == input[b])
 			{
-				s[i] = end[j];
+				rot[a] = output[b];
 				break;
 			}
-
-		}
-		i++;
-	}
-	return (s);
+	return (rot);
 }
