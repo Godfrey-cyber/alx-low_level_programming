7f79392e4badfc9f28a4babda8420c25613e158c
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-04 01:34:37 -0700

    appends the src string to the dest string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,8 @@
  * _strncat - this function that concatenates two strings except that
  * it will use at most n bytes from src.
  * @dest: char to check
+ * @src: char to check
+ * @n: input value
  * number of bytes from src
  * Description: This will append the src string to the dest string
  * Return: Return a pointer to the resulting string dest
