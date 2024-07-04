cf68a4b15dcf82ed1bedd7a43ead253c0d2c6ba0
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-04 00:53:41 -0700

    appends the src string to the dest string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - this function that concatenates two strings.
+ * Return: Returns a pointer to the resulting string dest
+ * @dest: char to check
+ * @src: char to check
+ * Description: This will append the src string to the dest string
+ *  Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != "\0"; 1++)
+	{
+	}
+	for (j = 0; src[i] != "\0"; j++; i++)
+	{
+		dest[i] = sc[j];
+	}
+
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
+
+	return (dest);
+}
