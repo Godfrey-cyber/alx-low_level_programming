d267789bb8a89899324360fbdd1925782baf18b0
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-04 01:44:17 -0700

    appends the src string to the dest string-4

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strncat - this function that concatenates two strings except that
- * it will use at most n bytes from src.
+ * _strncat - this function that concatenates two strings.
  * @dest: char to check
  * @src: char to check
- * @n: input value
- * number of bytes from src
+ * @n: number of bytes from src
  * Description: This will append the src string to the dest string
- * Return: Return a pointer to the resulting string dest
+ * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, newstring;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
+		i++;
 	}
-	for (newstring = 0; src[newstring] != 0; newstring++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	for (j = 0; j < n && b < newstring; j++, i++)
-	{
-		dest[a] = src[b];
-	}
+	dest[i] = '\0';
 	return (dest);
 }
 
