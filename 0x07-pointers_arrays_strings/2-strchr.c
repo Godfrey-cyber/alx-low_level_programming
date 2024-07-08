5a75767592cbe3194e23e230691fde5e231a88ef
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-08 13:51:42 -0700

    hello

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int x = 0;
+	int x;
 	
-	while (s[x] != 0)
+	for (x = 0; s[x] != '\0' ; x++)
 	{
 		if (s[x] == c)
-			return (s + x);
+		return (&s[x]);
 		x++;
 	}
-	if (c == 0)
-		return (s + x);
 	return (0);
 }
