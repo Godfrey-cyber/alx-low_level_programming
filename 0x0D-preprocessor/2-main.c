a55bd8ee8e1ec4efe589fba0a9b136aa7bccc50e
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-22 12:11:39 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - header file that defines a macro named PI
+ *
+ * @void: no parameters
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
