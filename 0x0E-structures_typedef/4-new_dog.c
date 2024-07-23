ea4f90845ea919d112d21a60e81f0c27b998c350
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-22 19:52:12 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,21 +12,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	/* dog_t is the new name of dog */
+	/* two variables to know the size of the variables I need to copy */
 	int namerunner, ownerrunner;
-	/* here we allocate the *new_dog function and test if is null or not */
+	/* allocate the *new_dog function and test if is null or not */
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (new_dog);
-	/* find out the size of the *name and *owner */
+	/* time to know the size of the *name and *owner */
 	for (namerunner = 0; name[namerunner]; namerunner++)
 	{
 	}
 	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
 	{
 	}
+	/* Add in new_dog the size of the variables name and owner */
+	/* dereference new_dog to each of the variables */
+	/* plus 1 to the runners because I need to save space for \0 too */
 	new_dog->name = malloc((namerunner + 1) * sizeof(char));
 	new_dog->owner = malloc((ownerrunner + 1) * sizeof(char));
+	/*check allocation was right if NULL free all the allocated memory*/
+	/* we freed memory from what is inside to the big function new_dog */
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
 		free(new_dog->name);
