5e695ff5df1d673411fb41524264237c9fb05cdb
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-25 12:28:16 -0700

    0x10-variadic_function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -21,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	nSum = 0;
 	for (i = 0; i < n; i++)
-	nSum += va_arg(ap, unsigned int);
+		nSum += va_arg(ap, unsigned int);
 	va_end(ap);
 	return (nSum);
 }
