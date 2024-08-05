3e724c8c76126eb58c2ad413378d8ddd8a1a47ce
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-08-05 12:20:03 -0700

    linked-list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,40 @@
 #include "lists.h"
 /**
- * add_node_end - a function that adds a new node at the end of a list_t list
+ * add_node_end -a function that adds a new node at the end of a list_t list
  * @head: a double pointer to list_t
- * @str: a pointer to the first node in list_t
+ * @str: pointer to the first node in list_t
  * Return: a poitner to the new node or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int racer;
-	list_t *new-last_node;
-	list_t *first_node;
+	int runner;
+	list_t *new_end_node;
+	list_t *firstnode;
 
-	new-last_node = malloc(sizeof(list_t));
-	racer = 0;
-	if (new-last_node)
+	new_end_node = malloc(sizeof(list_t));
+	runner = 0;
+	if (new_end_node)
 	{
-		new-last_node->str = strdup(str);
-		if (!new-last_node->str || !str)
+		new_end_node->str = strdup(str);
+		if (!new_end_node->str || !str)
 		{
-			free(new-last_node);
+			free(new_end_node);
 			return (NULL);
 		}
-		while (new-last_node->str[racer])
-			racer++;
-		new-last_node->next = NULL;
-		new-last_node->len = rucer;
+		while (new_end_node->str[runner])
+			runner++;
+		new_end_node->next = NULL;
+		new_end_node->len = runner;
 		if (!*head)
 		{
-			*head = new-last_node;
-			return (new-last_node);
+			*head = new_end_node;
+			return (new_end_node);
 		}
-		first_node = *head;
-		while (first_node->next)
-			first_node = first_node->next;
-		first_node->next = new-last_node;
-		return (new-last_node);
+		firstnode = *head
+		while (firstnode->next)
+			firstnode = firstnode->next;
+		firstnode->next = new_end_node;
+		return (new_end_node);
 	}
 	return (NULL);
 }
