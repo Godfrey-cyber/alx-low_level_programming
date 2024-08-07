aa882c94ca2f0f72772de5f2b6b5864b73d433cd
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-08-07 01:31:11 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,53 +1,59 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * find_listint_loop - function that finds the loop in a linked list.
- * @head: pointer to the head of linked list.
- *
- * You are not allowed to use malloc, free or arrays.
- * You can only declare a maximum of two variables in your function.
- *
- * Return: The address of the node where the loop starts, or NULL if,
- * there is no loop.
- */
+* checker - helper function taht checks if stuff matches and if we in a loop
+* @head: the head node
+*
+* Description: this function will check if we are in a loop
+* Return: either Null or where we know a loop exists
+* A: We will make 2 pointers that are equal to the head
+*	kinda like DNA leading and lagging strand during replication
+* B: We make the leading jump 2 times. we have the if and else check
+*	there because if leading exists and we can go next, we do it
+* C: if we cant then that means there was no infinite loop so return NULL
+* D: So in this loop, if the lagging ever matched with the leading,
+*	we know a loop existed and that we should return that back
+*/
+
+listint_t *checker(listint_t *head)
+{
+	listint_t *lagging = NULL;
+	listint_t *leading = NULL;
+
+	lagging = leading = head;/* A */
+	while (lagging)
+	{
+		lagging = lagging->next;
+		leading = leading->next;/* B */
+		if (leading && leading->next)
+			leading = leading->next;
+		else/* C */
+			return (NULL);
+		if (lagging == leading)/* D */
+			return (leading);
+	}
+	return (NULL);
+}
+
+/**
+* find_listint_loop - finds where the loop in a linked list is
+* @head: the head node
+*
+* Description: this is wrong i havent gotten it yhet
+* Return: The address of the node where loop starts or null if none
+* A: While the checker of head is a value, if the returned leading is
+*	equal to the head, then we return the head because that is a loop
+* B: we check if the return value matches our head
+* C: we increment head and run the checker all over again.
+*/
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	/* Use of Floyd’s Cycle-Finding Algorithm */
-	/* let slow_ptr and fast_ptr be two node pointers pointing to the head node */
-	listint_t *slow_ptr = head, *fast_ptr = head;
-	/* if list is empty or has only one node without a loop return NULL */
-	if (head == NULL || head->next == NULL)
-		return (NULL);
-	/* in every iteration slow_ptr ptr moves ahead by one node */
-	slow_ptr = slow_ptr->next;
-	/* in every iteration fast_ptr ptr moves ahead by two nodes */
-	fast_ptr = fast_ptr->next->next;
-	/* search for a loop using slow_ptr and fast_ptr pointers */
-	while (fast_ptr && fast_ptr->next)
+	while (checker(head))/* A */
 	{
-		/**
-		 * slow_ptr and fast_ptr eventually meet at the same node, thus,
-		 * indicating that the linked list contains a loop.
-		 */
-		if (slow_ptr == fast_ptr)
-			break;
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
+		if (checker(head) == head)/* B */
+			return (head);
+		head = head->next;/* C */
 	}
-	/* if loop does not exists return NULL*/
-	if (slow_ptr != fast_ptr)
-		return (NULL);
-	/* if loop exists, start slow_ptr from head and fast_ptr from meet point */
-	slow_ptr = head;
-	while (slow_ptr != fast_ptr)
-	{
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next;
-	}
-	/* return slow_ptr */
-	return (slow_ptr);
+	return (NULL);
 }
-git add .
