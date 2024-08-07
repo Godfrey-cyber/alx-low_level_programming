0d3b944df233e37b77c262eb1ab639ccefb779bb
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-08-06 20:27:22 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_listint_safe - defining the function
+ * @head:
+ * Return: what the function returns
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
