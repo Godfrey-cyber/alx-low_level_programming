ea69f0a1ed665533850cb9fb5ac5d55ffa96d283
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-08-12 12:05:48 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
+
 /**
- * read_textfile - this function is responsible reads a text file 
- * and prints it to the POSIX standard output.
- * @filename: the file to be read.
- * @letters: the number of characters to be read
- * Description: read thea file
- * section header: the header of this function is holberton.h
- * Return: this return the number of chars.
+ * read_textfile - A function that rReads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, 1_read,2_write;
+	ssize_t open, read, write;
 	char *buffer;
 
 	if (filename == NULL)
@@ -20,26 +22,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 600);
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	1_read = read(fd, buffer, letters);
-	if (1_read == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+	open = open(filename, O_RDONLY);
+	read = read(o, buffer, letters);
+	write = write(STDOUT_FILENO, buffer, r);
 
-	2_write = write(STDOUT_FILENO, buffer, 1_read);
-	if (2_write == -1 || 2_write != 1_read)
+	if (open == -1 || read == -1 || write == -1 || write != read)
 	{
+		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(fd);
-	return (2_write);
+	close(open);
+
+	return (write);
 }
