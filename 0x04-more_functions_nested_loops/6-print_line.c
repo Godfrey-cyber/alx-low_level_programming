367f2ec815d41ab2c21d37b1a26e484587588708
New Name <godfreyndiritu26@gmail.com>
2024-11-16 05:01:29 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /**
   *print_line - draws a straight line in the terminal
   *only use _putchar function to print
-  *@n: number of times _ should be printed
+  *@n: an int given by main
   *line should end with a \n
   *if n is 0 or less, function should only print \n
   *Return: success
