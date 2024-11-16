1c29f76f17d022c9e9a03bd2e202ec17ddb0b63e
New Name <godfreyndiritu26@gmail.com>
2024-11-16 05:06:24 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 /**
-  *print_line - draws a straight line in the terminal
-  *only use _putchar function to print
-  *@n: an int given by main
-  *line should end with a \n
-  *if n is 0 or less, function should only print \n
-  *Return: success
-  */
+ * print_line - Draws a line with a specific length.
+ * @n: The number of underscores (_) to print.
+ *
+ * Description: This function draws a line by printing underscores.
+ * If n is less than or equal to 0, it does not print anything.
+ */
 void print_line(int n)
 {
     int index;
