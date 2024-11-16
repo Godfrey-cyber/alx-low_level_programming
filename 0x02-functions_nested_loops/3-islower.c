7da4fafcc5709a679e60380553eed013ce7510ae
New Name <godfreyndiritu26@gmail.com>
2024-11-16 07:35:37 -0800

    101-crackme_password

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _islower - is Shows 1 if the input is a lowercase character
- * @c: The character in ASCII code
- * Return - Returns 1 for lowercase. 0 for the rest.
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
  */
 int _islower(int c)
+
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
