e64e776a2311ce90e10b1ea70db8c95bbf9a3b52
New Name <godfreyndiritu26@gmail.com>
2024-11-16 05:36:06 -0800

    9-fizz_buzz.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- *  puts2 - prints the characters of the string
- * @str: char to check
- * Return: Always 0.
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
 void puts2(char *str)
 {
-	int string;
-	
-	for (string = 0; str[string] != '\0'; string++)
-	if (string % 2 == 0)
-		_putchar(str[string]);
-	_putchar('\n');
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
 }
