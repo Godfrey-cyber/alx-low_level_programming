1e4071dfd076bac349915e354f3a9fca4b6f6739
New Name <godfreyndiritu26@gmail.com>
2024-11-16 07:13:45 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * set_string - set  the value of a pointer to a char
- * @s: double pointer
- * @to: pointer to set
- * Description: this print the chessboard
- * Return: a char
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
  */
 void set_string(char **s, char *to)
 {
