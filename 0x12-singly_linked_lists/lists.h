147b9a5a28fba18c23185465046f349adf542e58
New Name <godfreyndiritu26@gmail.com>
2024-11-16 09:11:16 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,18 @@
-#ifndef _LIST_H
-#define _LIST_H
-#include <stdarg.h>
-#include <stdlib.h>
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+    * struct list_s - singly linked list
+    * @str: string - (malloc'ed string)
+    * @len: length of the string
+    * @next: points to the next node
+    * Description: singly linked list node structure for ALX project
+    */
+
 typedef struct list_s
 {
 	char *str;
